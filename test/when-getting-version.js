e6ba464d897aa1b9af3265e4d2ae25f33e9c9040
Arne Hassel <arne.hassel@gmail.com>
2014-12-25 17:18:48 +0100

    A very rough implementation of cli-support

--- test/when-getting-version.js
+++ test/when-getting-version.js
@@ -1,9 +1,25 @@
-var expect = require('chai').expect;
+var expect = require('chai').expect,
+		child_process = require('child_process'),
+		util = require('util');
 
 describe('When getting version', function () {
+	var packageVersion;
+
+	beforeEach(function () {
+		packageVersion = require('../package').version;
+	});
+
 	it('should fetch version from package', function () {
 		var ebookrVersion = require('../lib/ebookr').version();
-		var packageVersion = require('../package').version;
 		expect(ebookrVersion).to.equal(packageVersion);
+	});
+
+	describe('With CLI', function () {
+		it('should log version', function (done) {
+			child_process.exec('node ./lib/ebookr.js --version', function (error, stdout, stderr) {
+				expect(stdout).to.equal(util.format('ebookr v%s\n', packageVersion));
+				done();
+			});
+		})
 	});
 });
