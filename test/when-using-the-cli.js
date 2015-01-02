8136495bf7189e85cbebd37f1eab3b63debbc663
Arne Hassel <arne.hassel@gmail.com>
2015-01-02 16:37:18 +0100

    Moved CLI-logic into CLI-module

--- test/when-using-the-cli.js
+++ test/when-using-the-cli.js
@@ -13,37 +13,41 @@ describe('When using the CLI', function () {
 		mockedConsole = { log: sinon.spy() };
 		converter = { convertFile: sinon.spy() };
 		extensions = { extend: sinon.spy() };
-		metadata = require('../lib/ebookr/metadata');
-		metadata.loadYAML = sinon.spy();
+		metadata = { loadYAML: sinon.spy() };
 		return mockrequire('../lib/ebookr', {
 			'extend': require('extend'),
-			'./ebookr/cli': cli,
+			'./ebookr/cli': mockrequire('../lib/ebookr/cli', {
+				nopt: function () {
+					return cli;
+				},
+				process: { argv: 42 },
+				'./metadata': metadata,
+				'../util/console': mockedConsole
+			}),
 			'./ebookr/converter': converter,
-			'./ebookr/extensions': extensions,
-			'./ebookr/metadata': metadata,
-			'./util/console': mockedConsole
+			'./ebookr/extensions': extensions
 		});
 	};
 
 	it('should be able to log version', function () {
-		mockEbookr({ version: true }).cli();
+		mockEbookr({ argv: { remain: [] }, version: true }).cli();
 		expect(mockedConsole.log).to.have.been.calledWithMatch(/ebookr v/);
 	});
 
 	it('should be pass on args', function () {
-		var args = { files: ['test'] };
+		var args = { argv: { remain: ['test'] } };
 		mockEbookr(args).cli();
 		expect(converter.convertFile).to.have.been.calledWith(['test'], args);
 	});
 
 	it('should warn if no files given', function () {
 		expect(function () {
-			mockEbookr({ files: [] }).cli();
+			mockEbookr({ argv: { remain: [] }}).cli();
 		}).to.throw(/No file\(s\) given/);
 	});
 
 	it('should be able to pass metadata', function () {
-		var args = { files: ['test'], metadata: ['foo=42', 'bar'] };
+		var args = { argv: { remain: ['test'] }, metadata: ['foo=42', 'bar'] };
 		var ebookr = mockEbookr(args);
 		ebookr.cli();
 		expect(ebookr.metadata.get('foo')).to.equal('42');
@@ -52,12 +56,12 @@ describe('When using the CLI', function () {
 	});
 
 	it('should pass metadata as file', function () {
-		mockEbookr({files: ['test'], metadataFile: 'test.yaml'}).cli();
+		mockEbookr({ argv: { remain: ['test'] }, metadataFile: 'test.yaml'}).cli();
 		expect(metadata.loadYAML).to.have.been.calledWith('test.yaml');
 	});
 
 	it('should support adding extensions', function () {
-		mockEbookr({files: ['test'], 'extension': ['test']}).cli();
+		mockEbookr({ argv: { remain: ['test'] }, 'extension': ['test']}).cli();
 		expect(extensions.extend).to.have.been.calledWith('ebookr-test');
 	});
 });
